6ff86536e880380c88b370a420fd94545d105393
WangH <WangH@example.com>
2025-06-06 10:31:47 +0800

    bump bin to ver 0.14.17

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "description": "OneHub是基于one-api二次开发而来的LLM API 管理 & 分发系统，支持 OpenAI、Azure、Anthropic Claude、Google Gemini、DeepSeek。", 
     "home_url": "Module_onehub.asp", 
     "link": "https://github.com/everstu/Koolcenter_onehub", 
-    "md5": "5a3409ff36c66b521b19091506935dcf", 
+    "md5": "42aec4438d8931b37c039a5e0f6925b3", 
     "module": "onehub", 
     "tags": "网络 AI", 
     "title": "OneHub", 
